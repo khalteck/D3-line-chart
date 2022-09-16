6a8e884813f367f7001d254cf83b2c6ccdfd1931
Khalteck <khalidoyeneye@gmail.com>
2022-09-16 12:07:15 -0600

    added data

---
+++ data.js
@@ -0,0 +1,138 @@
+let DATA = [
+    {
+     "article": 3,
+     "code": 4,
+     "date": "2022-02-14",
+     "text": 4,
+     "todo": 0,
+     "total": 11
+    },
+    {
+     "article": 3,
+     "code": 4,
+     "date": "2022-02-25",
+     "text": 4,
+     "todo": 0,
+     "total": 11
+    },
+    {
+     "article": 3,
+     "code": 4,
+     "date": "2022-03-1",
+     "text": 4,
+     "todo": 1,
+     "total": 11
+    },
+    {
+     "article": 3,
+     "code": 4,
+     "date": "2022-03-14",
+     "text": 4,
+     "todo": 1,
+     "total": 11
+    },
+    {
+     "article": 3,
+     "code": 4,
+     "date": "2022-03-22",
+     "text": 4,
+     "todo": 2,
+     "total": 11
+    },
+    {
+     "article": 3,
+     "code": 4,
+     "date": "2022-04-05",
+     "text": 5,
+     "todo": 2,
+     "total": 12
+    },
+    {
+     "article": 3,
+     "code": 4,
+     "date": "2022-04-10",
+     "text": 5,
+     "todo": 3,
+     "total": 12
+    },
+    {
+     "article": 3,
+     "code": 4,
+     "date": "2022-05-3",
+     "text": 5,
+     "todo": 4,
+     "total": 12
+    },
+    {
+     "article": 3,
+     "code": 5,
+     "date": "2022-05-10",
+     "text": 6,
+     "todo": 4,
+     "total": 14
+    },
+    {
+     "article": 3,
+     "code": 5,
+     "date": "2022-05-24",
+     "text": 6,
+     "todo": 3,
+     "total": 14
+    },
+    {
+     "article": 3,
+     "code": 5,
+     "date": "2022-06-10",
+     "text": 6,
+     "todo": 3,
+     "total": 14
+    },
+    {
+     "article": 3,
+     "code": 5,
+     "date": "2022-06-28",
+     "text": 6,
+     "todo": 2,
+     "total": 14
+    },
+    {
+     "article": 3,
+     "code": 5,
+     "date": "2022-07-8",
+     "text": 6,
+     "todo": 3,
+     "total": 14
+    },
+    {
+     "article": 3,
+     "code": 5,
+     "date": "2022-08-14",
+     "text": 6,
+     "todo": 2,
+     "total": 14
+    },
+    {
+     "article": 3,
+     "code": 5,
+     "date": "2022-08-18",
+     "text": 6,
+     "todo": 1,
+     "total": 14
+    },
+    {
+     "article": 3,
+     "code": 5,
+     "date": "2022-09-14",
+     "text": 6,
+     "todo": 2,
+     "total": 14
+    },
+    {
+     "article": 3,
+     "code": 5,
+     "date": "2022-09-19",
+     "text": 6,
+     "todo": 1,
+     "total": 14
+    }
+   ]

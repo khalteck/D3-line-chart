dafa0afea1e381817386585f87b40bc414cc31cb
Khalteck <khalidoyeneye@gmail.com>
2022-09-16 19:29:25 -0600

    all done

--- data.js
+++ data.js
@@ -1,91 +1,91 @@
-let DATA = [
+const data = [
     {
-     "article": 3,
-     "code": 4,
+     "article": 1,
+     "code": 1,
      "date": "2022-02-14",
-     "text": 4,
+     "text": 1,
      "todo": 0,
-     "total": 11
+     "total": 3
     },
     {
-     "article": 3,
-     "code": 4,
+     "article": 2,
+     "code": 1,
      "date": "2022-02-25",
-     "text": 4,
+     "text": 1,
      "todo": 1,
-     "total": 11
+     "total": 4
     },
     {
-     "article": 3,
-     "code": 4,
+     "article": 2,
+     "code": 1,
      "date": "2022-03-1",
-     "text": 4,
+     "text": 0,
      "todo": 1,
-     "total": 11
+     "total": 3
     },
     {
-     "article": 3,
-     "code": 4,
+     "article": 1,
+     "code": 2,
      "date": "2022-03-14",
-     "text": 4,
+     "text": 2,
      "todo": 1,
-     "total": 11
+     "total": 5
     },
     {
      "article": 3,
-     "code": 4,
+     "code": 2,
      "date": "2022-03-22",
-     "text": 4,
+     "text": 2,
      "todo": 2,
-     "total": 11
+     "total": 7
     },
     {
-     "article": 3,
+     "article": 1,
      "code": 4,
      "date": "2022-04-05",
-     "text": 5,
+     "text": 4,
      "todo": 1,
-     "total": 12
+     "total": 9
     },
     {
      "article": 3,
-     "code": 4,
+     "code": 2,
      "date": "2022-04-10",
-     "text": 5,
+     "text": 3,
      "todo": 3,
-     "total": 12
+     "total": 8
     },
     {
-     "article": 3,
-     "code": 4,
+     "article": 2,
+     "code": 5,
      "date": "2022-05-3",
-     "text": 5,
+     "text": 3,
      "todo": 4,
-     "total": 12
+     "total": 10
     },
     {
      "article": 3,
      "code": 5,
      "date": "2022-05-10",
-     "text": 6,
+     "text": 3,
      "todo": 3.5,
-     "total": 14
+     "total": 11
     },
     {
-     "article": 3,
+     "article": 4,
      "code": 5,
      "date": "2022-05-24",
-     "text": 6,
+     "text": 4,
      "todo": 3,
-     "total": 14
+     "total": 13
     },
     {
-     "article": 3,
-     "code": 5,
+     "article": 2,
+     "code": 4,
      "date": "2022-06-10",
      "text": 6,
      "todo": 3,
-     "total": 14
+     "total": 12
     },
     {
      "article": 3,
@@ -97,42 +97,44 @@ let DATA = [
     },
     {
      "article": 3,
-     "code": 5,
+     "code": 7,
      "date": "2022-07-8",
      "text": 6,
      "todo": 3,
-     "total": 14
+     "total": 16
     },
     {
-     "article": 3,
-     "code": 5,
+     "article": 5,
+     "code": 6,
      "date": "2022-08-14",
      "text": 6,
      "todo": 2,
-     "total": 14
+     "total": 17
     },
     {
-     "article": 3,
+     "article": 5,
      "code": 5,
      "date": "2022-08-18",
-     "text": 6,
+     "text": 4,
      "todo": 1,
      "total": 14
     },
     {
-     "article": 3,
-     "code": 5,
+     "article": 2,
+     "code": 2,
      "date": "2022-09-14",
      "text": 6,
      "todo": 2,
-     "total": 14
+     "total": 10
     },
     {
-     "article": 3,
-     "code": 5,
+     "article": 1,
+     "code": 4,
      "date": "2022-09-19",
      "text": 6,
      "todo": 1,
-     "total": 14
+     "total": 11
     }
-   ]
+]
+
+export default data
